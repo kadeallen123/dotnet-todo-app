51c163dd7bca967f53712c6753fe4fc8bddf1e19
Kade Allen <kadeallenprofessional@gmail.com>
2023-10-25 08:42:17 +1300

    Added while loop, printing out todos

--- Program.cs
+++ Program.cs
@@ -7,5 +7,27 @@ public class Program
 	public static void Main(string[] args)
 	{
 		Console.WriteLine("Welcome to .NET Todo App!");
+		List<TodoItem> todos = new();
+		// REPL
+		while (true)
+		{
+			//TODO Clear Screen
+			Console.WriteLine("Todos:");
+			if (todos.Count == 0)
+			{
+				Console.WriteLine("No todos...");
+			}
+			else
+			{
+				foreach (var t in todos)
+				{
+					Console.WriteLine($" - {t.Description} - [{(t.IsComplete ? 'x' : ' ')}]");
+				}
+			}
+
+			Console.WriteLine("");
+			Console.Write("|> ");
+			var input = Console.ReadLine();
+		}
 	}
 }
