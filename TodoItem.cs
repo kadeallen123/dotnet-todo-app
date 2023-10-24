02229c69013972e55b8125edd6de5fc5500583a5
Kade Allen <kadeallenprofessional@gmail.com>
2023-10-25 09:18:41 +1300

    Updated TodoItem to create id property

--- TodoItem.cs
+++ TodoItem.cs
@@ -1,3 +1,7 @@
 namespace DotNetTodo;
 
-public record struct TodoItem(string Description, bool IsComplete = false);
+public record struct TodoItem(string Description, bool IsComplete = false)
+{
+	private static int _todoCount = 0;
+	public readonly int Id => _todoCount++;
+}
